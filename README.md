2a2f0e278706d269992e3dd50ef42267acd90dd8
mohammad <mh.heidari76@gmail.com>
2019-10-26 00:01:27 +0330

    create apis

---
+++ README.md
@@ -0,0 +1,1 @@
+# HW1 of Internet Engineering course
---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express')
+const app = express()
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const isPointInsidePolygon = require('point-in-polygon')
+
+app.use(bodyParser.json())
+
+let polygons = {}
+
+fs.readFile(`${__dirname}/data.json`,function(err,data){
+    if(err) throw err;
+    polygons  = JSON.parse(data.toString())
+})
+
+
+app.put('/gis/addpolygon',function(req,res,next){
+    polygons.features.push(req.body)
+    fs.writeFile(`${__dirname}/data.json`,JSON.stringify(polygons),(err)=>{
+       if(err) throw err 
+    })
+    res.sendStatus(200)
+})
+app.get('/gis/testpoint',function(req,res,next){
+    let result = { polygons: [] };
+    polygons.features.forEach(element => {
+        if (isPointInsidePolygon([req.query.long, req.query.lat], element.geometry.coordinates[0])) {
+            result.polygons.push(element.properties.name)
+        }
+    });
+    res.send(result)
+})
+
+app.listen(8000,() => console.log(`Example app listening on port ${8000}!`))
